b40771cc45d762648daf956a1e7a9a4ddda0426c
liutongqing@h3d.com.cn <liutongqing@h3d.com.cn>
2023-08-27 14:37:04 +0800

    TODO24

---
+++ src/0024.swap-nodes-in-pairs/0024.swap-nodes-in-pairs.go
@@ -0,0 +1,12 @@
+package leetcode
+
+
+import (
+	"github.com/luckyhappycat/leetcode-go/structures"
+)
+
+type ListNode = structures.ListNode
+
+func swapPairs(head *ListNode) *ListNode {
+	return nil
+}
---
+++ src/0024.swap-nodes-in-pairs/0024.swap-nodes-in-pairs_test.go
@@ -0,0 +1,55 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/luckyhappycat/leetcode-go/structures"
+)
+
+type Param struct {
+	head *ListNode
+}
+
+type Result struct {
+	want *ListNode
+}
+
+type Case struct {
+	param  Param
+	result Result
+}
+
+func TestSwapPairs(t *testing.T) {
+	cases := []Case{
+		{
+			param: Param{
+				head: structures.InitListNode([]int{1, 2, 3, 4}),
+			},
+			result: Result{
+				want: structures.InitListNode([]int{2, 1, 4, 3}),
+			},
+		},
+		{
+			param: Param{
+				head: structures.InitListNode([]int{}),
+			},
+			result: Result{
+				want: structures.InitListNode([]int{}),
+			},
+		},
+		{
+			param: Param{
+				head: structures.InitListNode([]int{1}),
+			},
+			result: Result{
+				want: structures.InitListNode([]int{1}),
+			},
+		},
+	}
+	for _, c := range cases {
+		got := swapPairs(c.param.head)
+		if !structures.EqualListNode(got, c.result.want) {
+			t.Errorf("swapPairs(%v) == %v, want %v", c.param.head, got, c.result.want)
+		}
+	}
+}
